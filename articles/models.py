33924e559309637f1998b90144848ad9e04610a2
an dong woo <akskwha@naver.com>
2022-11-27 16:22:01 +0900

    feat(articles): 게시판 기능/조회,수정,삭제,목록

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from imagekit.models import ProcessedImageField
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    image = ProcessedImageField()
--- articles/urls.py
+++ articles/urls.py
@@ -2,10 +2,12 @@
 from django.urls import path
 from . import views
 
-app_name="articles"
+app_name = "articles"
 
 urlpatterns = [
     path('',views.index, name="index"),
     path('create/',views.create, name="create"),
-    path('detail/',views.detail, name="detail"),
+    path('<int:pk>/',views.detail, name="detail"),
+    path('<int:pk>/update/',views.update, name="update"),
+    path('<int:pk>/delete/',views.delete, name="delete"),
 ]
