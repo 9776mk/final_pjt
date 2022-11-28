d6b95d4885d86833ac5653bb223780abdb980930
taehopark <qkrxogh7@gmail.com>
2022-11-28 10:06:33 +0900

    static경로설정

--- final_pjt/settings.py
+++ final_pjt/settings.py
@@ -132,7 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-
+STATIC_ROOT = "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
