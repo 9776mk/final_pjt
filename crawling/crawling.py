b889c4b82431ad618ff38242312616828e5ff52f
9776mk <9776mk@gmail.com>
2022-12-01 22:00:03 +0900

    test: 임시 커밋

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -58,73 +58,89 @@ for i in range(1, 31):
         driver.implicitly_wait(1)
         time.sleep(1)
 
-        count = driver.find_elements(By.CLASS_NAME, "css-gv0s7n")
-        print(f"전체 개수 : {len(count)}")
+        c = driver.find_elements(By.CLASS_NAME, "css-gv0s7n")
+        count = len(c)
 
-        #         level = i
+        # level = i
         # 문제 번호
         # 한 페이지에 최대 50문제 1~50까지
         # 첫 번째 //*[@id="__next"]/div/div[4]/div[2]/div[1]/table/tbody/tr[5]/td[1]/div/div/div/span/a/span
         # 50 번째 //*[@id="__next"]/div/div[4]/div[2]/div[1]/table/tbody/tr[50]/td[1]/div/div/div/span/a/span
-        num_ = []
-        for k in range(1, count + 1):
-            prob_num = driver.find_elements(
-                By.XPATH,
-                f'//*[@id="__next"]/div/div[4]/div[2]/div[1]/table/tbody/tr[{k}]/td[1]/div/div/div/span/a/span',
-            )
-            for i in prob_num:
-                num_.append(i.text)
-        print(len(num_))
+        # num_ = []
+        # for k in range(1, count + 1):
+        #     prob_num = driver.find_elements(
+        #         By.XPATH,
+        #         f'//*[@id="__next"]/div/div[4]/div[2]/div[1]/table/tbody/tr[{k}]/td[1]/div/div/div/span/a/span',
+        #     )
+        #     for i in prob_num:
+        #         num_.append(i.text)
+        # print(num_)
+        # ==================================================================== #
 
         # title
         # 첫 번째 //*[@id="__next"]/div/div[4]/div[2]/div[1]/table/tbody/tr[1]/td[2]/span/div/div[1]/span[1]/div/a/span
         # 마지막 //*[@id="__next"]/div/div[4]/div[2]/div[1]/table/tbody/tr[50]/td[2]/span/div/div[1]/span[1]/div/a/span
-        title_ = []
-        for i in range(1, count + 1):
-            title = driver.find_elements(
-                By.XPATH,
-                f'//*[@id="__next"]/div/div[4]/div[2]/div[1]/table/tbody/tr[{i}]/td[2]/span/div/div[1]/span[1]/div/a/span',
-            )
-            for value in title:
-                # print(value.text)
-                title_.append(value.text)
-
-        print(len(title_))
-        print(title_)
+        # title_ = []
+        # for i in range(1, count + 1):
+        #     title = driver.find_elements(
+        #         By.XPATH,
+        #         f'//*[@id="__next"]/div/div[4]/div[2]/div[1]/table/tbody/tr[{i}]/td[2]/span/div/div[1]/span[1]/div/a/span',
+        #     )
+        #     for value in title:
+        #         # print(value.text)
+        #         title_.append(value.text)
+        # print(len(title_))
+        # print(title_)
+        # ==================================================================== #
 
         # 카테고리
         # 클릭해서 전부 열기
-        # 처음 : //*[@id="__next"]/div/div[4]/div[2]/div[1]/table/tbody/tr[1]/td[2]/span/div/div[1]/span[2]/div/button
-        # 마지막 : //*[@id="__next"]/div/div[4]/div[2]/div[1]/table/tbody/tr[50]/td[2]/span/div/div[1]/span[2]/div/button
-        category_ = [[] for _ in range(count)]
-
-        for i in range(1, count + 1):
-            for_category = driver.find_elements(
-                By.XPATH,
-                '//*[@id="__next"]/div/div[4]/div[2]/div[1]/table/tbody/tr[{i}]/td[i]/span/div/div[1]/span[2]/div/button',
-            )
-
-            for j in for_category:
-                # i.click() element not interactable 에러 발생
-                j.send_keys(Keys.ENTER)
-                # 크롤링
-
-                for k in j:
-                    # print(i.text)
-                    category_[k].append(i.text)
-
         # for_category = driver.find_elements(By.CLASS_NAME, "css-gv0s7n")
         # for i in for_category:
         #     # i.click() element not interactable 에러 발생
         #     i.send_keys(Keys.ENTER)
         #     # 크롤링
+        # ==================================================================== #
 
+        # 태그 열기 위한 버튼 클릭
+        # for k in range(1, count + 1):
+        #     abc = driver.find_elements(
+        #         By.XPATH,
+        #         f'//*[@id="__next"]/div/div[4]/div[2]/div[1]/table/tbody/tr[{k}]/td[2]/span/div/div[1]/span[2]/div/button',
+        #     )
+        #     abc.send_keys(Keys.Enter)
+
+        #     # 버튼 클릭 후
+        #     category_ = [[] for _ in range(count)]
+        #     category = driver.find_elements(By.CLASS_NAME, "css-18la3yb")
+        #     for i in category:
+        #         category_[k].append(i.text)
+        #         print(category_)
+
+        # category_ = [[] for _ in range(count)]
         # category = driver.find_elements(By.CLASS_NAME, "css-18la3yb")
         # for i in category:
         #     # print(i.text)
         #     category_.append(i.text)
+        # print(category_)
 
-        print(category_)
+        # 처음 : //*[@id="__next"]/div/div[4]/div[2]/div[1]/table/tbody/tr[1]/td[2]/span/div/div[1]/span[2]/div/button
+        # 마지막 : //*[@id="__next"]/div/div[4]/div[2]/div[1]/table/tbody/tr[50]/td[2]/span/div/div[1]/span[2]/div/button
+        category_ = [[] for _ in range(count)]
+        for i in range(1, count + 1):
+            for_category = driver.find_elements(
+                By.XPATH,
+                '//*[@id="__next"]/div/div[4]/div[2]/div[1]/table/tbody/tr[{i}]/td[i]/span/div/div[1]/span[2]/div/button',
+            )
+            for j in for_category:
+                # i.click() element not interactable 에러 발생
+                j.send_keys(Keys.ENTER)
+        #         # 크롤링
+        #         for k in j:
+        #             # print(i.text)
+        #             category_[k].append(i.text)
+        #     print(category_)
+        # print(category_)
 
 
 driver.close()
