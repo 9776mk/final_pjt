5156a1b4ebb3be35a3daf166e4b330081428f37e
taehopark <qkrxogh7@gmail.com>
2022-12-02 11:56:41 +0900

    Stack multi

--- accounts/forms.py
+++ accounts/forms.py
@@ -23,25 +23,35 @@ class ProfileForm(forms.ModelForm):
 
     class Meta:
         model = Profile
-        fields = ['image', 'nickname', 'github_id', 'boj_id', 'language', 'mbti', 'intro']
+        fields = [
+            "image",
+            "nickname",
+            "github_id",
+            "boj_id",
+            "language",
+            "stack",
+            "mbti",
+            "intro",
+        ]
         labels = {
-            'nickname': '닉네임',
-            'language': '사용 언어',
-            'mbti': 'MBTI',
-            'image': '프로필 사진',
-            'intro': '소개글',
-            'github_id': 'Github 아이디', 
-            'boj_id': '백준 아이디',
+            "nickname": "닉네임",
+            "language": "사용 언어",
+            "mbti": "MBTI",
+            "image": "프로필 사진",
+            "intro": "소개글",
+            "github_id": "Github 아이디",
+            "boj_id": "백준 아이디",
+            "stack": "기술스텍",
         }
 
 
 class GuestbookArticleForm(forms.ModelForm):
     class Meta:
         model = GuestbookArticle
-        fields = ['content']
+        fields = ["content"]
 
 
 class GuestbookCommentForm(forms.ModelForm):
     class Meta:
         model = GuestbookComment
-        fields = ['content']
+        fields = ["content"]
