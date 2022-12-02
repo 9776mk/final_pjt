9f22e36255793bed6ce31a3d81f8292eeeeb95a9
Subin <pomodoro5292@gmail.com>
2022-12-02 17:56:16 +0900

    feat: 쪽지 기능

--- notes/urls.py
+++ notes/urls.py
@@ -5,6 +5,7 @@ app_name = "notes"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("sent/", views.sent, name="sent"),
     path("send/", views.send, name="send"),
     path("<int:pk>/", views.detail, name="detail"),
     path("<int:pk>/delete/", views.delete, name="delete"),
