448559b4ae3bc0a7b90b2094c2d9b75ff0b46519
9776mk <9776mk@gmail.com>
2022-12-04 23:51:53 +0900

    test: 저장 완료

--- algorithm/models.py
+++ algorithm/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 
-class BJData(models.Model):
+class BJData_1(models.Model):
     level = models.IntegerField()
     number = models.IntegerField()
     title = models.CharField(max_length=50)
-    category = models.CharField(max_length=20)
+    tags = models.CharField(max_length=20)
--- crawling/crawling2.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "final_pjt.settings")
-import django
-
-django.setup()
