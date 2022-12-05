afdc6dbfc787270d69641b81c9bacf5a1e0fbd75
9776mk <9776mk@gmail.com>
2022-12-05 14:05:08 +0900

    fix: crawling 태그 없을 때 발생하는 오류

--- algorithm/views.py
+++ algorithm/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 
 def index(request):
-    return render(request, "algorithm/index.html")
+    br = BJData_br.objects.order_by("pk")
+    si = BJData_si.objects.order_by("pk")
+    context = {
+        "br": br,
+        "si": si,
+    }
+    return render(request, "algorithm/index.html", context)
